63b962e0a70cb81d7848e94db229715d2139f13a
Raphael <oi.sucesso@gmail.com>
2019-04-17 01:36:49 -0300

    Citartech service or cache mongodb
    Se o serviço Citartech falhar ao buscar os dados de raw country codes o cache entra em ação.

--- webserver/src/bo/ApiBO.js
+++ webserver/src/bo/ApiBO.js
@@ -1,26 +1,38 @@
 const Promise = require('bluebird');
 const Citartech = require('../service/CitartechService');
+const CountryCodesStore = require('../store/CountryCodesStore');
 
 class ApiBO {
     
     static getCountryCodes(type, sort = 'country', order = 'ascending') {
-        return Citartech.getRawCountryCodes()
-            .then(function (resp) {
-                let 
-                    lines     = resp.split(/\n/),
-                    newLines  = lines.slice(3, (lines.length - 2)  ),              
-                    list      = [],
-                    ascending = order.toLowerCase().trim() !== 'descending'
-                ;
+        return new Promise(function (ok, fail) {
+            
+            Citartech.getRawCountryCodes()
+                .then(function (resp) {
+                    let 
+                        lines     = resp.split(/\n/),
+                        newLines  = lines.slice(3, (lines.length - 2)  ),              
+                        list      = []
+                    ;
+
+                    for (let i = 0; i < newLines.length; i++) {
+                        let [ code, country ] = newLines[i].split(/\s\W*/);
+                        let data = { code, country };
+                        CountryCodesStore.saveOrUpdate(data);
+                        list.push(data);
+                    }
+                    console.log('** get CountryCodes for Citartech service **');
+                    ok(list);
+                })
+                .catch(function (err) {                    
+                    console.log('** get CountryCodes for CACHE mongodb **');
+                    CountryCodesStore.getAll().then(ok, fail);
+                });
                 
-                for (let i = 0; i < newLines.length; i++) {
-                    let [ code, country ] = newLines[i].split(/\s\W*/);
-                    list.push({ code, country });
-                }
-                
-                return new Sortable(list).sortPicker(sort.toLowerCase().trim(), ascending);                
-            });
-        ;
+        })
+        .then(function (list) {
+            return new Sortable(list).sortPicker(sort.toLowerCase().trim(), order.toLowerCase().trim() !== 'descending');       
+        });
     }
     
 }
