e35f12ca7cd2278150ba7c50b7e193ee894c2dfe
Raphael <oi.sucesso@gmail.com>
2019-04-17 01:35:05 -0300

    BUGFIX return fail()
    BUGFIX if (error) return fail(new Error(error));

--- webserver/src/service/CitartechService.js
+++ webserver/src/service/CitartechService.js
@@ -1,16 +1,18 @@
 const Promise = require('bluebird');
 const request = require("request");
 
+const URL = 'https://raw.githubusercontent.com/citartech/job-vacancies/master/assets/country-codes.txt'
+
 const Citartech = {
     
     getRawCountryCodes: function () {
         return new Promise(function (ok, fail) {
                 request({
                     method: 'GET',
-                    url: 'https://raw.githubusercontent.com/citartech/job-vacancies/master/assets/country-codes.txt',
+                    url: URL,
                 }, 
                 function (error, res, body) {                        
-                    if (error) fail(new Error(error));
+                    if (error) return fail(new Error(error));
 
                     if(res.statusCode != '200') 
                         fail(new Error({
