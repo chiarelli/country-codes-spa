b8bbc0971c1647e3e7350ba6a3f841a09c287ed8
Raphael <oi.sucesso@gmail.com>
2019-04-19 14:02:31 -0300

    Invalidate Routes to develop

--- webserver/src/webrouter.js
+++ webserver/src/webrouter.js
@@ -7,10 +7,12 @@ router.get( '/', webC.act_home );
 router.get( '/table', webC.act_get_table );
 
 const devC = require('./controller/DevController');
-router.get( '/api/dev/message', devC.act_ola_mundo );
-router.get( '/api/dev/create-country-codes', devC.act_create_model );
-router.get( '/api/dev/model-country-codes', devC.act_model_country_codes );
 
 router.get( '/api/country-codes', devC.act_get_country_codes );
+
+//router.get( '/api/dev/message', devC.act_ola_mundo );
+//router.get( '/api/dev/create-country-codes', devC.act_create_model );
+//router.get( '/api/dev/model-country-codes', devC.act_model_country_codes );
+
 
 
